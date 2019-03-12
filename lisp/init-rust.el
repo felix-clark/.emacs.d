6c2f8fedd747b99c8c4af5c631df057173855511
Felix Clark <mfclark3690@gmail.com>
2019-03-12 17:57:16 -0400

    note about flycheck-rust not working

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -19,24 +19,27 @@
 ;; easy building with `C-c C-c C-*` where *={b|r|t} for `cargo {build|run|test}`
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
-;; make C-c <tab> not just auto-indent the whole file but also apply rustfmt
-(add-hook 'rust-mode-hook
-	  (lambda ()
-	    (local-set-key (kbd "C-c TAB") #'rust-format-buffer)))
-;; TODO: set up "C-M-\" on a selection to do the same thing
-;; could also use rust-format-on-save
-
 ;; use racer with company-mode for tab auto-completion
 ;; (add-hook 'rust-mode-hook #'company-mode) ;; should already be a part of prog-mode
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+;; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup) ;; might need to install through Cask
 
- ;; rustup binaries PATH; doesn't appear necessary
-;; (setq racer-cmd "~/.cargo/bin/racer")
-;; point the the rust source installed with `rustup component add rust-src`
-(setq racer-rust-src-path (concat
-			   ;; get rustsrc location and delete trailing '\n'
-			   (substring (shell-command-to-string "rustc --print sysroot") 0 -1)
-			   "/lib/rustlib/src/rust/src"))
+;; wrap the rest in a lambda for speed?
+(add-hook 'rust-mode-hook
+	  '(lambda ()
+	     ;; make C-c <tab> not just auto-indent the whole file but also apply rustfmt
+	     (local-set-key (kbd "C-c TAB") #'rust-format-buffer)
+	     ;; TODO: set up "C-M-\" on a selection to do the same thing
+	     ;; could also use rust-format-on-save
+	     
+	     ;; use rustup binaries PATH; doesn't appear necessary
+	     ;; (setq racer-cmd "~/.cargo/bin/racer")
+	     ;; point the the rust source installed with `rustup component add rust-src`
+	     (setq racer-rust-src-path (concat
+					;; get rustsrc location and delete trailing '\n'
+					(substring (shell-command-to-string "rustc --print sysroot") 0 -1)
+					"/lib/rustlib/src/rust/src"))
+	     ))
 
 (provide 'init-rust)
