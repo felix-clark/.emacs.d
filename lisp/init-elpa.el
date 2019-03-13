4afa7d625dd866e58fa319eac1856447440142a2
Felix Clark <mfclark3690@gmail.com>
2019-03-13 13:59:24 -0400

    unstable melpa and maybe-require-package

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,5 +1,11 @@
+;;; init-elpa.el --- package management
+;;; Commentary:
+;;; Code:
+
 (require 'package)
 
+;; there are fancier versions of these functions in purcell's example
+;; that allow version requirements
 (defun require-package (package)
   "Install given PACKAGE if it was not installed before."
   (if (package-installed-p package)
@@ -9,9 +15,21 @@
 	(package-refresh-contents))
       (package-install package))))
 
+(defun maybe-require-package (package)
+ "Try to install given PACKAGE and if unsuccessful return nil and print a warning message."
+ (condition-case err
+     (require-package package)
+   (error
+    (message "Could't install optional package `%s': %S" package err)
+    nil)))
+
+;; some packages like flycheck-rust are not in MELPA Stable
+;; when changing this, probably need to run package-refresh-contents
 (add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/") t)
+	     ;; '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
 (provide 'init-elpa)
+;;; init-elpa.el ends here
