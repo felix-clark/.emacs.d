ca25e505155de24d555eeb46c751a3d430070cf6
Felix Clark <mfclark3690@gmail.com>
2019-03-13 13:57:54 -0400

    lisp and haskell

--- init.el
+++ init.el
@@ -2,10 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
+;; debuging
+;(setq debug-on-error t)
+
 ;; look in the "lisp" directory for additional custom macros
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+;; let flycheck find the emacs config files
+(setq-default flycheck-emacs-lisp-load-path 'inherit)
+
 ;; byte-compile everything that hasn't already been for speed
+;; in emacs 25 this can likely be replaced by auto-compile
 (byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
 
 ;; custom lisp functions
@@ -27,8 +34,10 @@
 (require 'init-company)
 
 ;; load specific language configurations
+(require 'init-lisp)
 (require 'init-python)
 (require 'init-rust)
+(require 'init-haskell)
 
 ;; load other file type configurations
 (require 'init-csv)
