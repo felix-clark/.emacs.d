384465a8ac2a5f01a6efba014d9dffdef821bd00
Felix Clark <mfclark3690@gmail.com>
2019-03-13 14:27:57 -0400

    projectile and smex

---
+++ lisp/init-navigation.el
@@ -0,0 +1,21 @@
+;;; init-navigation.el --- utility for browsing and searching
+;;; Commentary:
+;;; Code:
+
+;; projectile for navigation within projects
+(when (maybe-require-package 'projectile)
+  (add-hook 'after-init-hook 'projectile-mode)
+  ;; Shorter modeline
+  (setq-default projectile-mode-line-prefix " Proj")
+  (after-load 'projectile
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  (maybe-require-package 'ibuffer-projectile))
+
+;; smex to improve M-x
+(when (maybe-require-package 'smex)
+  ;; Change path for ~/.smex-items
+  (setq-default smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+  (global-set-key [remap execute-extended-command] 'smex))
+
+(provide 'init-navigation)
+;;; init-navigation.el ends here
