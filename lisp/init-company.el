10b79403063c514d2d6dd5f22535b47a2b87bec8
Felix Clark <mfclark3690@gmail.com>
2019-03-13 13:58:41 -0400

    updates to company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,11 +1,17 @@
-;; company-mode for autocompletion
+;;; init-company.el --- for auto-completion
+;;; Commentary:
+;;; Code:
 (require 'init-elpa)
 
 (require-package 'company)
 (require 'company)
+
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
 
 (setq company-tooltip-align-annotations t)
 (add-hook 'prog-mode-hook 'company-mode)
 (define-key prog-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 
 (provide 'init-company)
+;;; init-company.el ends here
