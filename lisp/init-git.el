98a70ff8e835872132575bb4108200b16a235385
Felix Clark <mfclark3690@gmail.com>
2019-03-19 16:56:40 -0400

    ido git

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -6,6 +6,8 @@
 (require 'init-elpa)
 
 (require-package 'magit)
+;; for ido compatibility
+(setq magit-completing-read-function 'magit-ido-completing-read)
 
 (provide 'init-git)
 ;;; init-git.el ends here
