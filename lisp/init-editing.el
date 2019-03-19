d76db7e667a70b883d1f32ae84782d792dd6c62d
Felix Clark <mfclark3690@gmail.com>
2019-03-19 14:10:17 -0400

    single-line commenting with no region selected

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -10,6 +10,21 @@
 ;; always show column number (line-number-mode is on by default)
 (setq column-number-mode t)
 
+;; Original idea from
+;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at the end of the line,
+        then comment current line.
+        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'comment-dwim-line)
+;; see emacs wiki on "CommentingCode" for alternative that comments full lines containing region a la eclipse
+
 ;; set flyspell for spellcheck on by default
 (require 'flyspell)
 (add-hook 'text-mode-hook 'flyspell-mode)
