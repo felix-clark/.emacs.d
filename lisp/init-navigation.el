f8c89f36ab6a57c83645a58141bee164373e650d
Felix Clark <mfclark3690@gmail.com>
2019-03-20 22:30:00 -0400

    moving to use-package

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -4,10 +4,13 @@
 (require 'init-elpa)
 
 ;; recentf to use <up> to browse recent files
-(require 'recentf)
-(setq recentf-save-file (concat user-emacs-directory ".recentf"))
-(recentf-mode 1)
-(setq recentf-max-menu-items 40)
+(use-package recentf
+  :init
+  (recentf-mode)
+  :config
+  (setq recentf-save-file (concat user-emacs-directory ".recentf"))
+  (setq recentf-max-menu-items 40)
+  )
 
 ;; ;; ido: interactive do. try ivy instead.
 ;; (require 'ido)
@@ -28,63 +31,73 @@
 ;; (require-package 'ido-completing-read+)
 ;; (ido-ubiquitous-mode 1)
 
-; try ivy over ido
-(require-package 'ivy)
-;; turn it on
-(ivy-mode 1)
-;; use recommended beginner configuration
-(setq ivy-use-virtual-buffers t) ; possibly needed for recentf
-(setq ivy-count-format "(%d/%d) ")
-(setq enable-recursive-minibuffers t) ;; suggested on github but not manual
-;; github suggestions
-(require-package 'swiper)
-(global-set-key (kbd "\C-s") 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-;; having trouble with counsel: ivy-make-magic-action is void?
-(require-package 'counsel)
-(counsel-mode) ;; enable several basic overrides
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-;; find file in git repo:
-(global-set-key (kbd "C-c g") 'counsel-git)
-;; grep within repo:
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag) ; we don't have ag installed now?
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; try ivy over ido
+(use-package ivy
+  :init (ivy-mode)
+  :config
+  (setq ivy-use-virtual-buffers t) ; possibly needed for recentf
+  (setq ivy-height 20) ;; optional
+  (setq ivy-count-format "(%d/%d) ")
+  :bind
+  (("C-c C-r" . ivy-resume)
+   ("<f6>" . ivy-resume))
+  )
+;; (setq enable-recursive-minibuffers t) ;; suggested on github but not manual, actually might be causing some annoying behavior
+(use-package swiper
+  :bind ("C-s" . swiper)
+  )
+
+(use-package counsel
+  :init
+  ;; overrides many common key bindings (like find-file)
+  (counsel-mode)
+  :bind (
+	 ("<f2> u" . counsel-unicode-char)
+	 ;; find file in git repo:
+	 ("C-c g" . counsel-git)
+	 ;; grep within repo:
+	 ("C-c j" . counsel-git-grep)
+	 ("C-c k" . counsel-ag) ; we don't have ag installed now?
+	 ("C-x l" . counsel-locate)
+	 ("C-S-o" . counsel-rhythmbox)
+	 )
+  :config
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+  )
 
 
 ;; use ibuffer interface for switching buffers to override list-buffers
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-;; projectile for navigation within projects
-(when (maybe-require-package 'projectile)
-  ;; (add-hook 'after-init-hook 'projectile-mode)
-  ;; Shorter modeline
-  ;; (setq-default projectile-mode-line-prefix " Proj")
-  ;; (after-load 'projectile
-  ;;   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-  
+(use-package projectile
+  :init
   ;; use projectile mode in standard commands instead of setting separate bindings
   ;; this can cause some issues, e.g. https://github.com/bbatsov/projectile/issues/994
-  (add-hook 'after-init-hook 'projectile-global-mode)
+  (projectile-mode)
+  :config
+  ;; use ivy with projectile
   (setq projectile-completion-system 'ivy)
-  (when (maybe-require-package 'counsel-projectile)
-    (setq counsel-projectile-mode t))
-  (maybe-require-package 'ibuffer-projectile)
-  (setq projectile-completion-system 'ivy))
+  )
+
+(use-package counsel-projectile
+  :init
+  (counsel-projectile-mode)
+  )
+
+(use-package ibuffer-projectile)
+  
 
 ;; amx is an active fork of smex for improved M-x utility
 ;; it automatically detects if ido or ivy is setup and if so it uses that backend
 ;; it has a sensible default for save items
-(require-package 'amx)
-(amx-mode)
-;; set one or both of these to nil to reduce a bit of delay
-;; (setq 'amx-ignored-command-matchers nil)
-;; (setq 'amx-show-key-bindings nil)
-
+(use-package amx
+  :init
+  (amx-mode)
+  ;; :config
+  ;; set one or both of these to nil to reduce a bit of delay
+  ;; (setq 'amx-ignored-command-matchers nil)
+  ;; (setq 'amx-show-key-bindings nil)
+  )
 
 (provide 'init-navigation)
 ;;; init-navigation.el ends here
