dfe95cbeb38ff5a425b496207200e5055088297a
Felix Clark <mfclark3690@gmail.com>
2019-03-28 09:12:31 -0400

    a decent-looking wrapping in org mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -11,5 +11,10 @@
 (global-set-key (kbd "C-c b") 'org-switchb)
 (setq org-log-done t)
 
+;; turn on line wrapping and indentation, which for some reason is disabled by default.
+(add-hook 'org-mode-hook '(lambda ()
+                            (visual-line-mode)
+                            (org-indent-mode)))
+
 (provide 'init-org)
 ;;; init-org.el ends here
