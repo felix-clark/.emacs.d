056cd4ffda7d0465252aba4727a7e3e2318db3e1
Felix Clark <mfclark3690@gmail.com>
2019-05-01 21:01:12 -0400

    use-package, and commenting troublesome code for now

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,7 +1,7 @@
 ;;; init-haskell.el --- Haskell utilities -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; uses intero for completion and flycheck, mostly from Purcell.
-;;; auto-complete sems slow, check if company mode is conflicting
+;;; auto-complete seems slow, check if company mode is conflicting
 ;;; Code:
 
 
@@ -10,76 +10,79 @@
   :bind (:map haskell-mode-map
 	      ("C-c h" . hoogle)
 	      ("C-o" . open-line))
-  
+  ;; this hook causes an infinite loop
+  ;; :hook (haskell-mode (turn-on-haskell-indentation
+  ;; 		       haskell-auto-insert-module-template))
+  :mode (("\\.[gh]s\\'" . haskell-mode)
+	 ;; the default is ghci-script-mode;
+	 ;; not sure what advantage haskell-mode in .ghci is
+	 ("\\.ghci\\'" . haskell-mode)
+	 ("\\.l[gh]s\\'" . literate-haskell-mode)
+	 ("\\.hsc\\'" . haskell-mode))
   )
 
+;; (use-package haskell-cabal
+;;   :after (haskell-mode
+;;           company)
+;;   :bind (:map haskell-cabal-mode ("C-c C-c" . haskell-compile))
+;; )
 
+;; cannot be found
+;; (use-package haskell-doc
+;;   :after haskell-mode)
 
-;; (require-package 'haskell-mode)
-
-(when (maybe-require-package 'intero)
-  (after-load 'haskell-mode
-	      (intero-global-mode)
-	      (add-hook 'haskell-mode-hook 'subword-mode)
-	      (add-hook 'haskell-mode-hook 'eldoc-mode))
-  (after-load 'haskell-cabal
-	      (add-hook 'haskell-cabal-mode 'subword-mode)
-	      (define-key haskell-cabal-mode-map (kbd "C-c C-l") 'intero-restart))
-  ;; (after-load 'intero
-  ;; 	      ;; prevent sanityinc/counsel-search-project binding from being overridden
-  ;; 	      (define-key 'intero-map-mode (kbd "M-?") nil)
-  ;; 	      (after-load 'flycheck
-  ;; 			  (flycheck-add-next-checker 'intero
-  ;; 						     '(warning . haskell-hlint))
-  ;; 			  )
-  ;; 	      )
+(use-package hindent
+  :init (require-package 'hindent)
+  :after haskell-mode
+  :config
+  (setq hindent-reformat-buffer-on-save t)
+  :hook (haskell-mode . hindent-mode)
   )
 
-;; add a new extension to activate haskell-mode
-;; the default is ghci-script-mode; not sure what advantage of this is
-(add-auto-mode 'haskell-mode "\\.ghci\\'")
-
-;; indentation
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
-;; source code help
-(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
-
-;; could have hindent in here
-
-;; (after-load 'haskell-mode
-;; 	    (define-key haskell-mode-map (kbd "C-c h") 'hoogle)
-;; 	    (define-key haskell-mode-map (kbd "C-o") 'open-line))
+;; ;; intero for full IDE features, though it is failing to autoload
+;; (use-package intero
+;;   :init (require-package 'intero)
+;;   :after (haskell-mode)
+;; 	  ;; haskell-cabal) ;; don't use this right now
+;;   :hook
+;;   (haskell-mode . (intero-global-mode
+;; 		   subword mode
+;; 		   eldoc-mode))
+;;   ;;  is this redundant?
+;;   ;; (haskell-cabal-mode . subword-mode)
+;;   ;; cabal mode not working?
+;;   ;; :bind (:map haskell-cabal-mode ("C-c C-l" . intero-restart))
+;;   )
 
 (use-package page-break-lines
   :init (require-package 'page-break-lines)
+  :after haskell-mode
   :config
   (push 'haskell-mode page-break-lines-modes)
   )
-;; (after-load 'page-break-lines
-;; 	    (push 'haskell-mode page-break-lines-modes))
 
-(define-minor-mode stack-exec-path-mode
-  "If this is a stack project, set `exec-path' to the path \"stack exec\" would use."
-  nil
-  :lighter ""
-  :global nil
-  (if stack-exec-path-mode
-      (when (and (executable-find "stack")
-		 (locate-dominating-file default-directory "stack.yaml"))
-	(setq-local
-	 exec-path
-	 (seq-uniq
-	  (append (list (concat (string-trim-right (shell-command-to-string "stack path --local-install-root")) "/bin"))
-		  (parse-colon-path
-		   (replace-regexp-in-string "[\r\n]+\\'" ""
-					     (shell-command-to-string "stack path --bin-path"))))
-	  'string-equal))
-					;(add-to-list (make-local-variable 'process-environment) (format "PATH=%s" (string-join exec-path path-separator)))
-	)
-    (kill-local-variable 'exec-path)))
+;; ;; stack stuff can cause emacs to hang for a long time if it's not set up right
+;; (define-minor-mode stack-exec-path-mode
+;;   "If this is a stack project, set `exec-path' to the path \"stack exec\" would use."
+;;   nil
+;;   :lighter ""
+;;   :global nil
+;;   (if stack-exec-path-mode
+;;       (when (and (executable-find "stack")
+;; 		 (locate-dominating-file default-directory "stack.yaml"))
+;; 	(setq-local
+;; 	 exec-path
+;; 	 (seq-uniq
+;; 	  (append (list (concat (string-trim-right (shell-command-to-string "stack path --local-install-root")) "/bin"))
+;; 		  (parse-colon-path
+;; 		   (replace-regexp-in-string "[\r\n]+\\'" ""
+;; 					     (shell-command-to-string "stack path --bin-path"))))
+;; 	  'string-equal))
+;; 					;(add-to-list (make-local-variable 'process-environment) (format "PATH=%s" (string-join exec-path path-separator)))
+;; 	)
+;;     (kill-local-variable 'exec-path)))
 
-(add-hook 'haskell-mode-hook 'stack-exec-path-mode)
+;; (add-hook 'haskell-mode-hook 'stack-exec-path-mode)
 
 (provide 'init-haskell)
-;;; init-haskel.el ends here
+;;; init-haskell.el ends here
