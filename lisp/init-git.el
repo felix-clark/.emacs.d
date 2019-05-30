9e02e3da12a0b3bf9851bd845153aada86156e59
Felix Clark <mfclark3690@gmail.com>
2019-05-30 13:28:33 -0400

    version check for magit

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -5,11 +5,15 @@
 
 (require 'init-elpa)
 
-(require-package 'magit)
-;; for ido compatibility
-;; (setq magit-completing-read-function 'magit-ido-completing-read)
-;; use ivy now
-(setq magit-completing-read-function 'magit-ivy-completing-read)
+(when (version<= "25" emacs-version)
+  (use-package magit
+    :init (require-package 'magit)
+    :config
+    ;; for ido compatibility
+    (setq magit-completing-read-function 'magit-ivy-completing-read)
+    :after (ivy)
+    )
+  )
 
 (provide 'init-git)
 ;;; init-git.el ends here
