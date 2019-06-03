a1e8f27754f71171872ebcfee04388312d79acf6
Felix Clark <mfclark3690@gmail.com>
2019-06-03 10:58:12 -0400

    fixing tex

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -2,12 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'company-auctex)
-  (company-auctex-init))
-
-;; need to make TAB the company completion key
-;; no need for indenting in latex so use complete-or-cycle
-(define-key LaTeX-mode-map (kbd "TAB") #'company-complete-common-or-cycle)
+(use-package company-auctex
+  :init (require-package 'company-auctex)
+  :defer t
+  :hook (LaTeX-mode . company-auctex-init)
+  ;; this tab behavior is actually annoying
+  ;; :bind (:map LaTeX-mode-map
+  ;; 	      ("TAB" . company-complete-common-or-cycle)
+  ;; 	      )
+  )
 
 (provide 'init-tex)
 ;;; init-tex.el ends here
