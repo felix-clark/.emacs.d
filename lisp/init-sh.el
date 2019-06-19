f30b1637562a2243e725c3c456610a7835bb826d
Felix Clark <mfclark3690@gmail.com>
2019-06-19 11:55:15 -0400

    shell scripting

---
+++ lisp/init-sh.el
@@ -0,0 +1,17 @@
+;;; init-sh.el --- Shell scripting functions
+;;; Commentary:
+;;; Most shell functionality is built-in, so this will not be large.
+;;; Code:
+
+(use-package company-shell
+  :init
+  (require-package 'company-shell)
+  :after company
+  :config
+  (push 'company-shell company-backends)
+  (push 'company-shell-env company-backends)
+  (push 'company-fish-shell company-backends)
+  )
+
+(provide 'init-sh)
+;;; init-sh.el ends here
