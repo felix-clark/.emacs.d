c1ea8635769a6c7e1bb6ed6a4d6b9a6ed80e30b0
Felix Clark <mfclark3690@gmail.com>
2019-11-12 12:55:16 -0500

    dockerfile mode

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -4,11 +4,17 @@
 
 (require 'init-elpa)
 
+;; weather integration
 (use-package wttrin
   :commands (wttrin)
   :init
   (require-package 'wttrin)
   (setq wttrin-default-cities '("New York City"))
+  )
+
+;; dockerfile editing
+(use-package dockerfile-mode
+  :init (require-package 'dockerfile-mode)
   )
 
 (provide 'init-misc)
