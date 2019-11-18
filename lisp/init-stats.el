18489a4a4b2fbdc49179f40fa6c5cdeb8dc039a3
Felix Clark <mfclark3690@gmail.com>
2019-11-18 17:54:10 -0500

    Fixing up then-R-operator, at least for X-window mode.

--- lisp/init-stats.el
+++ lisp/init-stats.el
@@ -9,11 +9,27 @@
   )
 
 (use-package ess
-  :init (require-package 'ess)
+  :init
+  (require-package 'ess)
   :defer t
+  ;; Define the pipe operator, which does not have a shortcut in ESS.
+  (defun then-R-operator ()
+    "R: 'then' pipe operator %>%"
+    (interactive)
+    (just-one-space 1)
+    (insert "%>%")
+    (reindent-then-newline-and-indent))
   ;; Bind M-- to assignment operator as in Rstudio. This is typically bound to
   ;; negative-argument, but C-M-- is also bound to negative-argument.
-  :bind ("M--" . ess-insert-assign)
+  ;; C-S-m for the pipe operator, corresponding to the shortcut in Rstudio,
+  ;; interferes with carriage return in interactive mode.
+  ;; The "C->" shortcut works in an X-window session but not in no-window mode.
+  ;; I can probably do most R coding locally, so this is probably fine. See:
+  ;; https://emacs.stackexchange.com/questions/1020/problems-with-keybindings-when-using-terminal
+  :bind (("M--" . ess-insert-assign)
+         ("C->" . then-R-operator))
+  ;; ess-set-style is not recognized for some reason.
+  ;; :config (ess-set-style 'RStudio)
   )
 
 (provide 'init-stats)
